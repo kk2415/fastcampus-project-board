1b345c54b0bd1e3540b5179183231b2c79fde357
Kim kyunghee <kkh2415@naver.com>
2022-07-12 16:22:32 +0900

    #7 - 도메인 설계 및 엔티티 작성

---
+++ fastcampus/src/main/java/com/fatcampus/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.fatcampus.projectboard.repository;
+
+import com.fatcampus.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
